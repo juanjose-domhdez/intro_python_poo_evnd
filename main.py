1a219cfd797adef62331a38d1a47da17a968866c
Juan Jose Dominguez Hernandez <jujodomhdez@gmail.com>
2026-05-15 22:43:07 -0600

    Segundo commit local

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def main():
     print("Hello, Word!")
+    print("Hola de nuevo, Mundo!")
+
+def suma(a,b):
+    return a + b
+
+a = int(input("Ingresa un número: "))
+b = int(input("Ingresa otro número: "))
+Resultado =suma(a,b)
+print(Resultado)
 
 if __name__ == "__main__":
     main()
