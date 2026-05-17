f9ca04d2edfd6edebf4051fc0db9fafd1a6a38bd
Juan Jose Dominguez Hernandez <jujodomhdez@gmail.com>
2026-05-16 19:40:09 -0600

    Tercer commit

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 def main():
     print("Hello, Word!")
     print("Hola de nuevo, Mundo!")
+    resultado = suma()
+    print(f"La suma de los dos numeros es {resultado}")
 
-def suma(a,b):
-    return a + b
-
-a = int(input("Ingresa un número: "))
-b = int(input("Ingresa otro número: "))
-Resultado =suma(a,b)
-print(Resultado)
+def suma():
+    num1 = int(input("Ingrese el primer numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
+    return num1 + num2
 
 if __name__ == "__main__":
     main()
