5416f4d0dbc4eb8f62f17596a77f8aa4f8f0457b
Juan Jose Dominguez Hernandez <jujodomhdez@gmail.com>
2026-05-16 20:35:51 -0600

    Multiplicacion de dos numeros

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ def main():
     print("Hola de nuevo, Mundo!")
     resultado = suma()
     print(f"La suma de los dos numeros es {resultado}")
+    resultado = multiplicacion()
+    print(f"El resultado de la multiplicacion es {resultado}")
 
 def suma():
+    print("Suma de dos numeros")
     num1 = int(input("Ingrese el primer numero: "))
     num2 = int(input("Ingrese el segundo numero: "))
     return num1 + num2
+
+def multiplicacion():
+    print("Multiplicacion de dos numeros")
+    num1 = int(input("Ingresa el primer numero: "))
+    num2 = int(input("Ingresa el segundo numero: "))
+    return num1 * num2
 
 if __name__ == "__main__":
     main()
